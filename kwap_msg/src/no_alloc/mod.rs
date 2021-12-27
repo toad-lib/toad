f6025efc458a83f6e55ed2c4377c4a65141846cc
Orion Kindel <cakekindel@gmail.com>
2021-12-27 16:36:12 -0700

    tweak docs

--- kwap_msg/src/no_alloc/mod.rs
+++ kwap_msg/src/no_alloc/mod.rs
@@ -1,11 +1,6 @@
 use tinyvec::ArrayVec;
 
-#[doc(inline)]
-pub use crate::from_bytes::*;
-#[doc(inline)]
-pub use crate::GetSize;
-#[doc(inline)]
-pub use crate::TryIntoBytes;
+pub use crate::{from_bytes::*, GetSize, TryIntoBytes};
 
 pub(crate) mod impl_from_bytes;
 pub(crate) mod impl_get_size;
