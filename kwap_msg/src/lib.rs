41a2343a6dd568ccde875e6fb55bb29ddcc5b2b5
cakekindel <cakekindel@users.noreply.github.com>
2021-12-27 03:22:23 +0000

    chore(release): kwap_msg@0.1.5

--- kwap_msg/src/lib.rs
+++ kwap_msg/src/lib.rs
@@ -18,7 +18,7 @@
 //! ### Deserializing from bytes
 //! ![chart](https://raw.githubusercontent.com/clov-coffee/kwap/improve_msg_readme/kwap_msg/docs/to_bytes.svg)
 
-#![doc(html_root_url = "https://docs.rs/kwap-msg/0.1.4")]
+#![doc(html_root_url = "https://docs.rs/kwap-msg/0.1.5")]
 /* TODO: make user-facing `kwap` crate and put this there
  * # `kwap`
  *
