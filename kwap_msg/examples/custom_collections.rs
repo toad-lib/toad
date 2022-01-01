28e728cf2b03ab3f155fbcb846754d45dc35c3e7
Orion Kindel <cakekindel@gmail.com>
2022-01-01 11:09:21 -0700

    fix example

--- kwap_msg/examples/custom_collections.rs
+++ kwap_msg/examples/custom_collections.rs
@@ -1,21 +1,8 @@
 use collection_heapless_vec::HeaplessVec;
-use collection_list::List;
 use kwap_common::{Array, GetSize, Reserve};
 use kwap_msg::*;
 
 fn main() {
-  type ListMsg = Message<List<u8>, List<u8>, List<Opt<List<u8>>>>;
-  let list_msg = ListMsg { code: Code { class: 2, detail: 5 },
-                           ty: Type(0),
-                           ver: Default::default(),
-                           id: Id(0),
-                           opts: List::Nil,
-                           payload: Payload(List::Nil),
-                           token: Token(Default::default()),
-                           __optc: Default::default() };
-  let bytes = list_msg.try_into_bytes::<List<u8>>().unwrap();
-  let _roundtrip = ListMsg::try_from_bytes(bytes).unwrap();
-
   type StackMsg = Message<HeaplessVec<u8, 16>, HeaplessVec<u8, 32>, HeaplessVec<Opt<HeaplessVec<u8, 32>>, 1>>;
   let stack_msg = StackMsg { code: Code { class: 2, detail: 5 },
                              ty: Type(0),
@@ -25,15 +12,61 @@ fn main() {
                              payload: Payload(Default::default()),
                              token: Token(Default::default()),
                              __optc: Default::default() };
-  let bytes = stack_msg.try_into_bytes::<List<u8>>().unwrap();
+  let bytes = stack_msg.try_into_bytes::<HeaplessVec<u8, 128>>().unwrap();
   let _roundtrip = StackMsg::try_from_bytes(bytes).unwrap();
 }
 
 pub(crate) mod collection_heapless_vec {
+  use std::{ops::{Index, IndexMut},
+            ptr};
+
+  use kwap_common::Insert;
+
   use super::*;
   #[derive(Default)]
   pub struct HeaplessVec<T: Default, const N: usize>(heapless::Vec<T, N>);
   impl<T: Default, const N: usize> Array<T> for HeaplessVec<T, N> {}
+
+  impl<T: Default, const N: usize> Insert<T> for HeaplessVec<T, N> {
+    fn insert_at(&mut self, index: usize, value: T) {
+      if index == self.0.len() {
+        self.push(value);
+        return;
+      }
+
+      let me: Self = unsafe { ptr::read(self as *const Self) };
+
+      let mut value_container = Some(value);
+
+      let buffer = me.into_iter()
+                     .enumerate()
+                     .fold(heapless::Vec::<T, N>::new(), |mut buf, (ix, val)| {
+                       if ix == index {
+                         buf.push(value_container.take().unwrap()).ok();
+                       } else {
+                         buf.push(val).ok();
+                       }
+
+                       buf
+                     });
+
+      *self = Self(buffer);
+    }
+  }
+
+  impl<T: Default, const N: usize> Index<usize> for HeaplessVec<T, N> {
+    type Output = T;
+    fn index(&self, index: usize) -> &Self::Output {
+      &self.0[index]
+    }
+  }
+
+  impl<T: Default, const N: usize> IndexMut<usize> for HeaplessVec<T, N> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+      &mut self.0[index]
+    }
+  }
+
   impl<T: Default, const N: usize> Reserve for HeaplessVec<T, N> {}
   impl<T: Default, const N: usize> GetSize for HeaplessVec<T, N> {
     fn get_size(&self) -> usize {
@@ -68,112 +101,6 @@ pub(crate) mod collection_heapless_vec {
 
     fn into_iter(self) -> Self::IntoIter {
       self.0.iter()
-    }
-  }
-}
-
-mod collection_list {
-  use std::fmt::Debug;
-
-  use kwap_msg::*;
-
-  use super::*;
-  #[derive(Clone, Debug)]
-  pub enum List<T: Debug + Clone> {
-    Cons(T, Box<List<T>>),
-    Nil,
-  }
-
-  impl<T: Debug + Clone> Default for List<T> {
-    fn default() -> Self {
-      List::Nil
-    }
-  }
-
-  impl<T: Debug + Clone> Array<T> for List<T> {}
-
-  impl<T: Debug + Clone> GetSize for List<T> {
-    fn get_size(&self) -> usize {
-      self.into_iter().count()
-    }
-
-    fn max_size(&self) -> Option<usize> {
-      None
-    }
-  }
-
-  impl<T: Debug + Clone> Reserve for List<T> {}
-
-  impl<T: Debug + Clone> List<T> {
-    pub fn cons(self, t: T) -> Self {
-      List::Cons(t, Box::from(self))
-    }
-  }
-
-  pub struct ListIntoIter<T: Debug + Clone> {
-    list: Box<Option<List<T>>>,
-  }
-
-  pub struct ListIter<'a, T: Debug + Clone> {
-    list: &'a List<T>,
-  }
-
-  impl<T: Debug + Clone> FromIterator<T> for List<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-      let mut items = iter.into_iter().collect::<Vec<_>>();
-      items.reverse();
-
-      items.into_iter().fold(List::<T>::Nil, |list, t| list.cons(t))
-    }
-  }
-
-  impl<'a, T: Debug + Clone> IntoIterator for &'a List<T> {
-    type Item = &'a T;
-    type IntoIter = ListIter<'a, T>;
-
-    fn into_iter(self) -> Self::IntoIter {
-      ListIter { list: self }
-    }
-  }
-
-  impl<T: Debug + Clone> IntoIterator for List<T> {
-    type Item = T;
-    type IntoIter = ListIntoIter<T>;
-
-    fn into_iter(self) -> Self::IntoIter {
-      ListIntoIter { list: Box::from(Some(self)) }
-    }
-  }
-
-  impl<T: Debug + Clone> Iterator for ListIntoIter<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<T> {
-      match self.list.take() {
-        | Some(List::Cons(t, next)) => {
-          self.list = Box::from(Some(*next));
-          Some(t)
-        },
-        | None | Some(List::Nil) => None,
-      }
-    }
-  }
-
-  impl<'a, T: Debug + Clone> Iterator for ListIter<'a, T> {
-    type Item = &'a T;
-    fn next(&mut self) -> Option<&'a T> {
-      match self.list {
-        | List::Cons(t, next) => {
-          self.list = &next;
-          Some(t)
-        },
-        | List::Nil => None,
-      }
-    }
-  }
-
-  impl<T: Clone + Debug> Extend<T> for List<T> {
-    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-      *self = self.clone().into_iter().chain(iter).collect();
     }
   }
 }
