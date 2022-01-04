fb3163727c96ddf5d8bcf89e704512f566f18af4
cakekindel <cakekindel@users.noreply.github.com>
2022-01-04 04:54:24 +0000

    chore(release): kwap@0.1.2

--- kwap/CHANGELOG.md
+++ kwap/CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### 0.1.2 (2022-01-04)
+
+
+### Features
+
+* **kwap:** add request type ([#39](https://github.com/clov-coffee/kwap/issues/39)) ([2fb2622](https://github.com/clov-coffee/kwap/commit/2fb262260f80455e3649f99d7be763015a269b2d))
+
 ### 0.1.1 (2022-01-02)
 
 
--- kwap/src/lib.rs
+++ kwap/src/lib.rs
@@ -23,7 +23,7 @@
 //! - Because UDP is a "connectionless" protocol, it offers no guarantee of "conversation" between traditional client and server roles. All the UDP transport layer gives you is a method to listen for messages thrown at you, and to throw messages at someone. Owing to this, CoAP machines are expected to perform both client and server roles (or more accurately, _sender_ and _receiver_ roles)
 //! - While _classes_ of status codes are the same (Success 2xx -> 2.xx, Client error 4xx -> 4.xx, Server error 5xx -> 5.xx), the semantics of the individual response codes differ.
 
-#![doc(html_root_url = "https://docs.rs/kwap/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/kwap/0.1.2")]
 #![cfg_attr(all(not(test), feature = "no_std"), no_std)]
 #![cfg_attr(not(test), forbid(missing_debug_implementations, unreachable_pub))]
 #![cfg_attr(not(test), deny(unsafe_code, missing_copy_implementations))]
