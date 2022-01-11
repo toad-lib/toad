aa617646a8b4e161d9602f5ce6e57ef8a3898bd3
Orion Kindel <cakekindel@gmail.com>
2022-01-11 00:54:01 -0700

    what

--- kwap/src/socket.rs
+++ kwap/src/socket.rs
@@ -22,7 +22,6 @@ pub trait Socket {
 
   /// Poll the socket for a datagram
   ///
-  ///
   fn poll(&self) -> Result<Option<ArrayVec<[u8; 1152]>>, Self::Error> {
     let mut buf = [0u8; 1152];
     let recvd = self.recv(&mut buf);
