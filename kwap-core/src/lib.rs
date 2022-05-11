8a090ea0b8f0042144eb0d1c0696ae05e122cfe6
Orion Kindel <cakekindel@gmail.com>
2022-05-11 14:27:01 -0700

    chore: mk kwap-core

---
+++ kwap-core/src/lib.rs
@@ -0,0 +1,25 @@
+//! `kwap-core`
+//!
+//! Low-level abstractions for kwap; the platformless CoAP implementation
+
+// docs
+#![doc(html_root_url = "https://docs.rs/kwap-core/0.0.0")]
+#![cfg_attr(any(docsrs, feature = "docs"), feature(doc_cfg))]
+// -
+// style
+#![allow(clippy::unused_unit)]
+// -
+// deny
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_copy_implementations)]
+#![cfg_attr(not(test), deny(unsafe_code))]
+// -
+// warnings
+#![cfg_attr(not(test), warn(unreachable_pub))]
+// -
+// features
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc as std_alloc;
