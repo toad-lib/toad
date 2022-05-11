f9c5e1b8c8b0d152b316d55a09c4f12e7714184f
Orion Kindel <cakekindel@gmail.com>
2022-05-11 14:21:44 -0700

    chore: add subcrate template

---
+++ tpl/src/lib.rs
@@ -0,0 +1,117 @@
+//! `kwap` is a Rust CoAP implementation that aims to be:
+//! - Platform-independent
+//! - Extensible
+//! - Approachable
+//!
+//! ## CoAP
+//! CoAP is an application-level network protocol that copies the semantics of HTTP
+//! to an environment conducive to **constrained** devices. (weak hardware, small battery capacity, etc.)
+//!
+//! This means that you can write and run two-way RESTful communication
+//! between devices very similarly to the networking semantics you are
+//! most likely very familiar with.
+//!
+//! ### Similarities to HTTP
+//! CoAP has the same verbs and many of the same semantics as HTTP;
+//! - GET, POST, PUT, DELETE
+//! - Headers (renamed to [Options](https://datatracker.ietf.org/doc/html/rfc7252#section-5.10))
+//! - Data format independent (via the [Content-Format](https://datatracker.ietf.org/doc/html/rfc7252#section-12.3) Option)
+//! - [Response status codes](https://datatracker.ietf.org/doc/html/rfc7252#section-5.9)
+//!
+//! ### Differences from HTTP
+//! - CoAP customarily sits on top of UDP (however the standard is [in the process of being adapted](https://tools.ietf.org/id/draft-ietf-core-coap-tcp-tls-11.html) to also run on TCP, like HTTP)
+//! - Because UDP is a "connectionless" protocol, it offers no guarantee of "conversation" between traditional client and server roles. All the UDP transport layer gives you is a method to listen for messages thrown at you, and to throw messages at someone. Owing to this, CoAP machines are expected to perform both client and server roles (or more accurately, _sender_ and _receiver_ roles)
+//! - While _classes_ of status codes are the same (Success 2xx -> 2.xx, Client error 4xx -> 4.xx, Server error 5xx -> 5.xx), the semantics of the individual response codes differ.
+
+// docs
+#![doc(html_root_url = "https://docs.rs/kwap/0.6.1")]
+#![cfg_attr(any(docsrs, feature = "docs"), feature(doc_cfg))]
+// -
+// style
+#![allow(clippy::unused_unit)]
+// -
+// deny
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_copy_implementations)]
+#![cfg_attr(not(test), deny(unsafe_code))]
+// -
+// warnings
+#![cfg_attr(not(test), warn(unreachable_pub))]
+// -
+// features
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc as std_alloc;
+
+#[doc(hidden)]
+pub mod todo;
+
+#[cfg(test)]
+pub(crate) mod test;
+
+/// Blocking rust CoAP client & server
+pub mod blocking;
+
+/// customizable retrying of fallible operations
+pub mod retry;
+
+/// responses
+pub mod resp;
+
+/// requests
+pub mod req;
+
+/// low-level coap behavior
+pub mod core;
+
+/// platform configuration
+pub mod platform;
+
+/// network abstractions
+pub mod net;
+
+/// `std`-only kwap stuff
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+pub mod std;
+
+mod option;
+pub use option::{ContentFormat, ToCoapValue};
+
+static mut ID: u16 = 0;
+static mut TOKEN: u64 = 0;
+
+// TODO(#79): Make token and ID generation use the Core's state and not mutable statics
+fn generate_id() -> kwap_msg::Id {
+  #[allow(unsafe_code)]
+  unsafe {
+    ID += 1;
+    kwap_msg::Id(ID)
+  }
+}
+
+// TODO(#79)
+fn generate_token() -> kwap_msg::Token {
+  #[allow(unsafe_code)]
+  unsafe {
+    TOKEN += 1;
+    kwap_msg::Token(TOKEN.to_be_bytes().into())
+  }
+}
+
+macro_rules! code {
+  (rfc7252($section:literal) $name:ident = $c:literal.$d:literal) => {
+    #[doc = kwap_macros::rfc_7252_doc!($section)]
+    #[allow(clippy::zero_prefixed_literal)]
+    pub const $name: kwap_msg::Code = kwap_msg::Code::new($c, $d);
+  };
+  (rfc7252($section:literal) $name:ident = $newtype:tt($c:literal.$d:literal)) => {
+    #[doc = kwap_macros::rfc_7252_doc!($section)]
+    #[allow(clippy::zero_prefixed_literal)]
+    pub const $name: $newtype = $newtype(kwap_msg::Code::new($c, $d));
+  };
+}
+
+pub(crate) use code;
