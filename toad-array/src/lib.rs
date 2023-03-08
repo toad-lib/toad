6f2f6fab49c63ef3f57974a8ba5c06e3242297d8
Orion Kindel <cakekindel@gmail.com>
2023-03-08 12:22:03 -0700

    fix(array): use toad-len

--- toad-array/src/lib.rs
+++ toad-array/src/lib.rs
@@ -34,70 +34,7 @@ use core::ops::{Deref, DerefMut};
 
 #[cfg(feature = "alloc")]
 use std_alloc::vec::Vec;
-
-/// Get the runtime size of some data structure
-pub trait Len {
-  /// The maximum number of elements that this data structure can acommodate.
-  const CAPACITY: Option<usize>;
-
-  /// Get the runtime size (in bytes) of a struct
-  ///
-  /// For collections this is always equivalent to calling an inherent `len` method.
-  ///
-  /// ```
-  /// use toad_common::GetSize;
-  ///
-  /// assert_eq!(vec![1u8, 2].get_size(), 2)
-  /// ```
-  fn len(&self) -> usize;
-
-  /// Check if the runtime size is zero
-  ///
-  /// ```
-  /// use toad_common::GetSize;
-  ///
-  /// assert!(Vec::<u8>::new().size_is_zero())
-  /// ```
-  fn is_empty(&self) -> bool {
-    self.len() == 0
-  }
-
-  /// Is there no room left in this collection?
-  ///
-  /// ```
-  /// use toad_common::GetSize;
-  ///
-  /// let array = tinyvec::ArrayVec::<[u8; 2]>::from([1, 2]);
-  ///
-  /// assert!(array.is_full())
-  /// ```
-  fn is_full(&self) -> bool;
-}
-
-#[cfg(feature = "alloc")]
-impl<T> Len for Vec<T> {
-  const CAPACITY: Option<usize> = None;
-
-  fn len(&self) -> usize {
-    self.len()
-  }
-
-  fn is_full(&self) -> bool {
-    false
-  }
-}
-
-impl<A: tinyvec::Array> Len for tinyvec::ArrayVec<A> {
-  const CAPACITY: Option<usize> = Some(A::CAPACITY);
-
-  fn len(&self) -> usize {
-    self.len()
-  }
-
-  fn is_full(&self) -> bool {
-    self.len() >= self.capacity()
-  }
-}
+use toad_len::Len;
 
 /// Create a data structure and reserve some amount of space for it to grow into
 ///
