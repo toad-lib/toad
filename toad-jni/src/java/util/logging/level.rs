d949c62dbe21618ccfb983925020de2a699faa6a
Orion Kindel <cakekindel@gmail.com>
2023-04-17 09:43:58 -0500

    feat: Level.from_log_level

--- toad-jni/src/java/util/logging/level.rs
+++ toad-jni/src/java/util/logging/level.rs
@@ -23,6 +23,27 @@ pub enum Level {
   Severe,
 }
 
+impl Level {
+  /// ```
+  /// use toad_jni::java::util::logging::Level;
+  ///
+  /// assert_eq!(Level::from_log_level(log::Level::Trace), Level::Finer);
+  /// assert_eq!(Level::from_log_level(log::Level::Debug), Level::Fine);
+  /// assert_eq!(Level::from_log_level(log::Level::Info), Level::Info);
+  /// assert_eq!(Level::from_log_level(log::Level::Warn), Level::Warning);
+  /// assert_eq!(Level::from_log_level(log::Level::Error), Level::Severe);
+  /// ```
+  pub fn from_log_level(lv: log::Level) -> Self {
+    match lv {
+      | log::Level::Error => Self::Severe,
+      | log::Level::Warn => Self::Warning,
+      | log::Level::Info => Self::Info,
+      | log::Level::Debug => Self::Fine,
+      | log::Level::Trace => Self::Finer,
+    }
+  }
+}
+
 impl java::Object for Level {
   fn upcast(e: &mut java::Env, jobj: java::lang::Object) -> Self {
     use Level::*;
