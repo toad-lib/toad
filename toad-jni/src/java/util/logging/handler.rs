139362edce84346f9c6fb8a9e783ed6bfd12bf1b
Orion Kindel <cakekindel@gmail.com>
2023-04-24 14:03:17 -0500

    feat: ConsoleHandler.setFormatter

--- toad-jni/src/java/util/logging/handler.rs
+++ toad-jni/src/java/util/logging/handler.rs
@@ -1,4 +1,4 @@
-use super::Level;
+use super::{Formatter, Level};
 use crate::java;
 
 /// `java.util.logging.Handler`
@@ -26,6 +26,13 @@ impl ConsoleHandler {
   pub fn set_level(&self, e: &mut java::Env, level: Level) {
     static SET_LEVEL: java::Method<ConsoleHandler, fn(Level)> = java::Method::new("setLevel");
     SET_LEVEL.invoke(e, self, level);
+  }
+
+  /// `void setFormatter(java.util.logging.Formatter)`
+  pub fn set_formatter(&self, e: &mut java::Env, f: Formatter) {
+    static SET_FORMATTER: java::Method<ConsoleHandler, fn(Formatter)> =
+      java::Method::new("setFormatter");
+    SET_FORMATTER.invoke(e, self, f);
   }
 
   /// Convert to parent class `Handler`
